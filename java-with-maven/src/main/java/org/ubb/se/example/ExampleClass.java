08cb9c3412eec719622d96deb2b172496a8895b8
Csaba Sulyok <csaba.sulyok@gmail.com>
2015-11-23 22:37:59 +0200

    jenkins: example project with maven and java

---
+++ java-with-maven/src/main/java/org/ubb/se/example/ExampleClass.java
@@ -0,0 +1,20 @@
+package org.ubb.se.example;
+
+/**
+ * Example class to demonstrate automatic testing and Jenkins reports using Java
+ * & Maven.
+ * 
+ * @author Csaba Sulyok
+ */
+public class ExampleClass {
+
+  private static final String THE_WORD = "bird";
+
+  public String whatsTheWord() {
+    return THE_WORD;
+  }
+
+  public boolean isTheWord(String referenceWord) {
+    return THE_WORD.equalsIgnoreCase(referenceWord.trim());
+  }
+}
