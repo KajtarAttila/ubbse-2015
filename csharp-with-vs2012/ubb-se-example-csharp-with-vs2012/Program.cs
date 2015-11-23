38bf70a0a1e6117eef2b2bd9599a54005a7a5ad3
Csaba Sulyok <csaba.sulyok@gmail.com>
2015-11-24 00:08:40 +0200

    jenkins: example vs2012 c# app

---
+++ csharp-with-vs2012/ubb-se-example-csharp-with-vs2012/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ubb_se_example_csharp_with_vs2012
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      System.Console.WriteLine("Hello, World!");
+    }
+  }
+}
