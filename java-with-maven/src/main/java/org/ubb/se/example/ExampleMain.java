3354382ca2d9e347b4efbee0baf36e200056c346
Csaba Sulyok <csaba.sulyok@gmail.com>
2015-11-25 17:23:20 +0200

    jenkins: appassembler plugin to java project

---
+++ java-with-maven/src/main/java/org/ubb/se/example/ExampleMain.java
@@ -0,0 +1,21 @@
+package org.ubb.se.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main entry point to example application.
+ * Contains some log messages to include an outside dependency.
+ * 
+ * @author Csaba Sulyok
+ */
+public class ExampleMain {
+  private static final Logger LOG = LoggerFactory.getLogger(ExampleMain.class);
+  
+  public static void main(String[] args) {
+    LOG.warn("= Main entry point =");
+    
+    ExampleClass example = new ExampleClass();
+    LOG.info("The {} is the word!", example.whatsTheWord());
+  }
+}
