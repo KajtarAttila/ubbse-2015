f06868ea9c281db10c3750c8d5d240979ac75f65
Csaba Sulyok <csaba.sulyok@gmail.com>
2015-12-14 16:38:31 +0200

    ELRONTVA

--- java-with-gradle/src/test/java/org/ubb/se/example/ExampleClassTest.java
+++ java-with-gradle/src/test/java/org/ubb/se/example/ExampleClassTest.java
@@ -23,7 +23,7 @@ public class ExampleClassTest {
   @Test
   public void whatsTheWordTest() {
     String theWord = example.whatsTheWord();
-    assertThat("bird", is(theWord));
+    assertThat("birz", is(theWord));
   }
 
   @Test
